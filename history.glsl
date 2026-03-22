c281d5f5bae0a19fd5ba6d37001cf9d795c013e7
Todd S <tstewart1@wpi.edu>
2026-03-22 18:24:18 -0400

    Update history.glsl

--- history.glsl
+++ history.glsl
@@ -4,13 +4,16 @@ void main () {
     vec3 color = black;
     float x = 0.3;
     float y = 0.05;
-    vec4 front = texture2D(channel0,  stN).rgba;
+    vec4 texture = texture2D(channel0,  stN).rgba;
+    vec3 hiker = black;
 
     color += circle(x, y, .5 * bands.z, 0.7) * white -0.5;
     color += circle(x, y, .7 * (1.1*bands.y), .5) * white*0.5 * bands.x * 5.;
     
+    hiker += circle(0.028, -0.02, 0.829, 0.) * bands.x * 0.1;
     
-    color = mix(color, vec3(front), 0.95);
+    
+    color = hiker + mix(color, vec3(texture), 0.95);
     
     vec3 fade = 1.0 - stN.x * 0.9 * black;
     
