d06e51b2448303f849c4ad1d0cffd88b527acfea
Todd S <tstewart1@wpi.edu>
2026-03-22 07:31:22 -0400

    pvt ref

---
+++ the_force-reference.js
@@ -0,0 +1,52 @@
+PI              = 3.14159;
+PI2             = 6.28318;
+uv()            = vec2(x, y); // This pixel on screen when
+                              // coordinate system is: width
+                              //  -width/height to width/height
+                              //, height -1.0 to 1.0;
+                              // center of canvas is origin
+uvN()             = vec2(x, y); // this pixel on screen when
+                              // coordinate system is: width
+                              // 0.0 to 1.0, height 0.0 to 1.0;
+                              // bottom-left of canvas is origin
+kale(vec2 p, float n) = float // Accepts UV-like space, and
+                              // divisions of PI. Returns a
+                              // kaleidoscope value
+rand(float x)   = float       // pseudo-random 0.0 to 1.0
+rand(vec2 x)    = float
+noise( float )  = float       // noise 0.0 to 1.0
+noise( vec2 )   = float
+noise( vec3 )   = float
+snoise( vec2 )  = float       // signed noise -1.0 to 0.0
+snoise( vec3 )  = float
+turbulence( vec2,
+            float octaves ) = float    //returns random values 0.0 to 1.0
+voronoi( vec2 ) = float       // returns 0.0 to 1.0 with a smooth
+                              // voronoi pattern
+voronoi( vec3 ) = vec3        // returns a vec3 voronoi in three dimensions
+fbm(float x, int it) = float  // Brownian motion function generator
+                              // second parameter is number of iterations
+fbm(vec2 x, int it) = float
+fbm(vec3 x, int it) = float
+rmf(vec2 x, int it) = float   // rigid multi-fractal generator
+                              // second parameter is number of iterations
+vrmf(vec2 x, int it) = float  // Voronoi version of rigid multi-fractal
+                              // second parameter is number of iterations
+vfbm(vec2 x, int it) = float  // Voronoi version of Brownian motion
+                              // second parameter is number of iterations
+
+hsv2rgb( vec3 ) = vec3        // convert hue, saturation, value to
+                              // red, green, blue
+rotate(vec2 pivot,
+       vec2(x, y),
+       float amount)
+
+box(vec2(x, y),
+    vec2(width, height),
+    float corner-roundness,
+    float edge-feathering);
+
+circle(float x,
+       float y,
+       float radius,
+       float edge-feathering)
