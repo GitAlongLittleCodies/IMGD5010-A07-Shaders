5d84a8cf7b55a31bdcb7a9200573541c10481515
Todd S <tstewart1@wpi.edu>
2026-03-22 07:55:53 -0400

    Update the_force-reference.js

--- the_force-reference.js
+++ the_force-reference.js
@@ -1,3 +1,61 @@
+=== GL ==
+gl_FragCoord = vec4(x, y, z, w) // fragment position within
+                                // frame buffer window coordinates
+
+gl_FragColor = vec4(r, g, b, a) // fragment color
+
+texture2D(tex, vec2) = vec4(r, g, b, a) // tex is a texture: channel0 ...
+                                        // channel3 or backbuffer
+
+
+  === MATH ===
+  // list of common gles math functions
+radians(x)      // degrees to radians
+degrees(x)      // radians to degrees
+sin(x)          // sine of angle
+cos(x)          // cosine of angle
+tan(x)          // tangent of angle
+asin(x)         // arc sine of angle
+acos(x)         // arc cosine of angle
+atan(y, x)      // arc tangent of (y, x)
+atan(y_over_x)  // arc tangent of y/x
+pow(x, y)       // x to y exponent; x^y
+exp(x)          // ex
+log(x)          // natural log
+exp2(x)         // 2x
+log2(x)         // log base 2
+sqrt(x)         // square root; x^(1/2)
+inversesqrt(x)  // inverse square root; 1/(x^(1/2))
+abs(x)          // absolute value
+sign(x)         // returns -1.0, 0.0, or 1.0
+floor(x)        // nearest integer less than x
+ceil(x)         // nearest integer greater than x
+fract(x)        // x - floor(x)
+mod(x, y)       // modulus
+min(x, y)       // minimum value
+max(x, y)       // maximum value
+clamp(x,        // keep x between minVal and maxVal
+      minVal,
+      maxVal)
+mix(x, y, a)    // linear blend of x and y using a. (lerp)
+step(edge, x)   // 0.0 if x less than edge, else 1.0
+smoothstep(edge0, // clip and smooth
+           edge1,
+           x)
+  
+=== SNIPPETS ===
+forf    = for(float i = 0.0; i < float; i++){
+          }
+fori    = for(int i = 0; i < int; i++){
+          }
+iff     = if(false){
+          }
+vc     = vec2
+vvc    = vec3
+ft     = float
+
+
+=== ADD ONS ==
 PI              = 3.14159;
 PI2             = 6.28318;
 uv()            = vec2(x, y); // This pixel on screen when
